d8d6520a1470116dcd561fec25520689f22b79dd
torvik <pless.victor@gmail.com>
2016-03-16 17:31:23 -0300

    layout_main

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,17 @@
 class AdminController < ApplicationController
   def index
      @total_orders = Order.count
+     @totalproducts = Product.count
+     @totalitens = LineItem.count
 
+     @order = Order.all
+     @line_items = LineItem.where("line_items.order_id= ?", @order_id)
+
+     @total_pedido = 0
+     @line_items.each do |line_items|
+      @ordernumber = @order.id
+      @total_pedido += line_items.total_price
+     end
 
 
 
