368721a51a3d9a3b9ccf12391b72e154940e73d3
torvik <pless.victor@gmail.com>
2016-03-16 01:05:05 -0300

    migration

--- db/migrate/20160316031020_create_stocks.rb
+++ db/migrate/20160316031020_create_stocks.rb
@@ -1,7 +1,7 @@
 class CreateStocks < ActiveRecord::Migration
   def change
     create_table :stocks do |t|
-      t.references :products, index: true, foreign_key: true
+      t.references :products, index: true
       t.integer :quantity
       t.boolean :active
 
