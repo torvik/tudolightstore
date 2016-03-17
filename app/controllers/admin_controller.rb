4f93bae74e0f198b72dd2ef02b76f469fa2b7688
torvik <pless.victor@gmail.com>
2016-03-16 21:55:27 -0300

    dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,13 @@ class AdminController < ApplicationController
      @total_orders = Order.count
      @totalproducts = Product.count
      @totalitens = LineItem.count
+     @totalstock = Stock.all.sum(:quantity)
   end
+
+        #@totalvendido = Sale.where(:user_id => current_user.id).sum(:amount)
+        #@valormediovenda = Sale.where(:user_id => current_user.id).average(:amount)
+        #@maiorvenda = Sale.where(:user_id => current_user.id).maximum(:amount)
+        #@menorvenda = Sale.where(:user_id => current_user.id).minimum(:amount)
 
      #@products = Product.where(:user_id => current_user.id).paginate(:page => params[:page], :per_page => 10).order(name: :asc, tamanho: :asc)
      #@totalproducts = Product.where(:user_id => current_user.id).count
