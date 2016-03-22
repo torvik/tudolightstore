d75ba6789f008c7dedb185b7e89c8da5f8e53d70
torvik <pless.victor@gmail.com>
2016-03-22 20:34:25 -0300

    ordem no js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-datepicker
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-datepicker
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
